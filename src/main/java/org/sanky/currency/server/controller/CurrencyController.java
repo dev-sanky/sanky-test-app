a974877ad16412acc26489abed09b2181ac04c2a
sanky <dev.sanky@gmail.com>
2019-04-27 09:00:03 -0500

    ds support

--- src/main/java/org/sanky/currency/server/controller/CurrencyController.java
+++ src/main/java/org/sanky/currency/server/controller/CurrencyController.java
@@ -1,10 +1,19 @@
 package org.sanky.currency.server.controller;
 
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.sanky.currency.server.service.CurrencyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @RestController
 public class CurrencyController {
@@ -12,9 +21,15 @@ public class CurrencyController {
 	@Autowired
 	CurrencyService currencyService;
 	
+	@Value("${spring.datasource.url}")
+  	private String dbUrl;
+
+  	@Autowired
+  	private DataSource dataSource;
+
 	private static final String DEFAULT_BASE_CURRENCY_CODE = "USD";
 	
-	@GetMapping("/sanky-test-app/base/{baseCurrencyCode}")
+	@GetMapping("/currency/base/{baseCurrencyCode}")
 	public String getLatestExchangeRates(@PathVariable String baseCurrencyCode){
 		
 		//if(currencyService.isValidApplicationId(applicationId)) {
@@ -27,4 +42,16 @@ public class CurrencyController {
 		//	return null;
 		//}
 	}
+
+	@Bean
+	public DataSource dataSource() throws SQLException {
+	  if (dbUrl == null || dbUrl.isEmpty()) {
+		return new HikariDataSource();
+	  } else {
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl(dbUrl);
+		return new HikariDataSource(config);
+	  }
+	}
+
 }
